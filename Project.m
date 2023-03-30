22df3cedfff65d3ee0221146bda923b397d7886f
Ankush Chohan <CHHANK001@myuct.ac.za>
2023-03-30 09:21:27 +0200

    Finished code

--- Project.m
+++ Project.m
@@ -1,3 +1,9 @@
+clc %Clears the command window
+% EEE3091 Project 
+% Authors:  Ankush Chohan (CHHANK001)
+%           Ashik John (JHNASH009)
+% Date of last revision: 30/03/2023
+
 % Section A Q1
 
 % Define variables
@@ -10,7 +16,7 @@ Prot = double([134.669, 88.924]);
 Vline = 380;
 f = 50;
 p = 3;
-Vp = double(Vline / sqrt(3));
+Vp = double(Vline / sqrt(3));       % Calculates phase voltage
 
 
 % Calculate Vth
@@ -40,8 +46,7 @@ disp(['Xth = ' num2str(Xth)])
 
 % find Ns and ws
 Ns = (120*f)/p;                    
-    %disp(['Ns  = ' num2str(Ns)])
-ws = Ns*(2*pi/60);
+ws = Ns*(2*pi/60);          % Ns in rad/s
 
 % Question 2 
 % Cerate s matrix
@@ -53,17 +58,18 @@ s = 0.01*a;
 % create Ns matrix
 f = flip(s);          
 n = s.*Ns;
-
+w = n.*(2*pi/60);
+disp(['w = ' num2str(w)])
     
 % Calc T
 Tm1 = 3*(1/ws).*((Vth(1)^2) ./ ((Rth(1)+R2(1) ./ (f).^2)+((Xth(1) + X2(1)).^2))).*(R2(1)./(f));
 Tm2 = 3*(1/ws).*((Vth(2)^2) ./ ((Rth(2)+R2(2) ./ (f).^2)+((Xth(2) + X2(2)).^2))).*(R2(2)./(f));
 
-% create figure 1 window with subplots
+% Create figure 1 window with subplots
 figure(1);
 subplot(3,1,1);
 % Plot Torque vs speed
-plot(n,Tm1, 'b-',n,Tm2,'r-')                    % plots are showing %Ns but need to be showing n 
+plot(n,Tm1, 'b-',n,Tm2,'r-')                   
 title('Torque vs Speed Characteristic')
 xlabel('Speed [rpm]')
 ylabel('Torque(Nm)')
@@ -73,8 +79,8 @@ legend('SE Motor', 'EE Motor')
 Z11 = R1(1) + complex(0, X1(1)) + (complex(0,Xm(1)).*((R2(1)./(f))+complex(0,X2(1)))) ./ ((R2(1)./(f)) + complex(0, Xm(1)+X2(1))); %eqn is correct but outputting wrong values
 Z12 = R1(2) + complex(0, X1(2)) + (complex(0,Xm(2)).*((R2(2)./(f))+complex(0,X2(2)))) ./ ((R2(2)./(f)) + complex(0, Xm(2)+X2(2)));
 
-disp(['Z11 = ' num2str(Z11)])
-%disp(['Z12 = ' num2str(Z12)])
+    %disp(['Z11 = ' num2str(Z11)])
+    %disp(['Z12 = ' num2str(Z12)])
 I11 = Vp ./ abs(Z11);           %Current in motor 1
 I12 = Vp ./ abs(Z12);           %Current in motor 2
 
@@ -83,7 +89,7 @@ subplot(3,1,2)
 plot(n,I11, 'b-',n,I12,'r-')
 title('Stator current vs Speed Characteristic')
 xlabel('Speed [rpm]')
-ylabel('Torque(Nm)')
+ylabel('Stator current(A)')
 legend('SE Motor', 'EE Motor')
 
 % Question 4
@@ -92,7 +98,7 @@ pf1 = cos(theta1);
 theta2 = atan(imag(Z12) ./ real(Z12));
 pf2 = cos(theta2);
 
-%plot pf vs %Ns
+%plot pf vs speed
 subplot(3,1,3)
 plot(n,pf1, 'b-',n,pf2,'r-')
 title('Power Factor vs Speed Characteristic')
@@ -102,19 +108,20 @@ legend('SE Motor', 'EE Motor')
 
 % Question 5 rotational loss excluded
 % Motor 1                           % Eqns from slide 21 Week 6
-Pin1 = 3*Vp*I11;                % Input power
-P1cu1 = 3*I11*R1(1);               % Stator copper loss
-P2cu1 = 3*I11*R2(1);               % Rotor copper loss
-Pag1 = P2cu1./s;                    % Airgap power
-Pshaft1 = Pin1 - P1cu1 - P2cu1;     % Shaft power
-disp(['P1cu1 = ' num2str(P1cu1)])
+Pin1 = 3*Vp*I11;                    % Input power
+P1cu1 = 3*(I11.^2)*R1(1);           % Stator copper loss
+P2cu1 = 3*(I11.^2)*R2(1);           % Rotor copper loss
+Pag1 = Pin1 - P1cu1;                % Airgap power
+Pshaft1 = Pag1 - P2cu1;             % Shaft power
+disp(['Pin1 = ' num2str(Pin1)])
+disp(['Pshaft1 = ' num2str(Pshaft1)])
 
 % Motor 2
-Pin2 = 3*Vp.*I12;                % Input power
-P1cu2 = 3.*I12*R1(2);               % Stator copper loss
-P2cu2 = 3.*I12*R2(2);               % Rotor copper loss
-Pag2 = P2cu2./s;                    % Airgap power
-Pshaft2 = Pin2 - P1cu2 - P2cu2;     % Shaft power
+Pin2 = 3*Vp.*I12;                      % Input power
+P1cu2 = 3.*(I12.^2)*R1(2);             % Stator copper loss
+P2cu2 = 3.*(I12.^2)*R2(2);             % Rotor copper loss
+Pag2 = Pin2 - P1cu2;                   % Airgap power
+Pshaft2 = Pag2 - P2cu2;                % Shaft power
 
 % Create figure 2 
 figure(2);
@@ -133,3 +140,47 @@ title('Motor 2 Powers vs Speed Characteristic')
 xlabel('Speed [rpm]')
 ylabel('P')
 legend('Pin', 'P1cu', 'P2cu', 'Pag', 'Pshaft')
+
+% Question 6
+
+% Motor 1
+n1 = Pshaft1./Pin1;        % Calculate efficiency
+
+% Plot efficiency vs speed for motor 1
+% Create figure 3
+figure(3)
+% Plot efficiency for motor 1
+subplot(2,1,1)
+plot(n,n1)
+title('Motor 1 Efficiency vs Speed Characteristic')
+xlabel('Speed [rpm]')
+ylabel('Eff')
+
+% Motor 2
+n2 = Pshaft2./Pin2;         % Calculate efficiency
+
+% Plot efficiency for motor 2
+subplot(2,1,2)
+plot(n,n2)
+title('Motor 2 Efficiency vs Speed Characteristic')
+xlabel('Speed [rpm]')
+ylabel('Eff')
+
+% Question 7
+
+k = 946.88 * 10^-6;     % Define constant variable
+Tlo = k.*(w.^2);        % Calculate load torque
+    %disp(['Tlo = ' num2str(Tlo)])
+
+% Create figure 4
+figure(4)
+
+% Plot Torque vs speed with load torque displayed aswell 
+plot(n,Tm1,n,Tm2,n, Tlo)                   
+title('Torque vs Speed Characteristic')
+xlabel('Speed [rpm]')
+ylabel('Torque(Nm)')
+legend('SE Motor', 'EE Motor','Tlo')
+
+
+
